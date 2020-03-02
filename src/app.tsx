b0243dd918be965d495ba05ce18e4850dec9cf90
Logan Bittner <lbittner@pdftron.com>
2020-03-02 12:04:46 -0800

    Minor CSS tweaks

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Viewer from './components/Viewer/Viewer';
 import Header from './components/Header/Header';
+import './app.scss';
 
 const App = () => {
   return (
-    <div>
+    <div className='app'>
       <Header />
       <Viewer />
     </div>
